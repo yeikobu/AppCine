408670ed7a93c9585eb8f7c87c6cfe5fb60b8834
Jacob Aguilar <aguilar.campos.jacob@gmail.com>
2022-01-31 21:45:16 -0300

    Initial Commit

---
+++ AppCine/AppCineApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppCineApp.swift
+//  AppCine
+//
+//  Created by Jacob Aguilar on 31-01-22.
+//
+
+import SwiftUI
+
+@main
+struct AppCineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
